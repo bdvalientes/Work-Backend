e24e41af858b5a84b4d8da7a7725cb0cc044367f
Brayan David Valiente Simbaqueba <bdvalientes@gmail.com>
2018-01-30 20:03:39 -0500

    Refactorizar
    se refactorizo el codigo, solo quedo el servidor

--- index.js
+++ index.js
@@ -1,71 +1,9 @@
 'use strict' 
 
-const express = require('express')
-const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const UserInfo = require('./models/UserInfo')
-
-const app = express()
+const app = require('./app')
 
 const port = process.env.PORT || 3001
- 
-app.use(bodyParser.urlencoded({ extended: false}))
-app.use(bodyParser.json())
-
-
-// Mostrar Todos Los Usuario 
-app.get('/api/UserInfo', (req,res)=> {
-    UserInfo.find({},(err,userinfo)=>{
-        if(err) return res.status(500).send({message: `Error al realizar la peticion ${err}`})
-        if(!userinfo) return res.status(404).send({message: `No Existe Usuarios`})
-
-        res.status(200).send({userinfo})
-    })
-})
-
-
-//Mostrar Usuario Especifico
-app.get('/api/UserInfo/:UserId', (req,res)=> {
-    let UserId = req.params.UserId
-    
-
-    UserInfo.findById(UserId,(err,userInfo)=> {
-        if(err) return res.status(500).send({message: `Error al realizar la peticion ${err}`})
-        if(!UserId) return res.status(404).send({message : `El Usuario No Existe`})
-
-        res.status(200).send({userInfo})
-    })
-})
-
-
-//Crear Usuario
-app.post('/api/UserInfo',(req,res)=>{
-    console.log('POST /api/UserInfo')
-    console.log(req.body)
-
-    let User = new UserInfo()
-    User.email = req.body.email
-    User.displayName = req.body.displayName
-    User.phone = req.body.phone
-    //User.puntuation = req.body.puntuation
-    User.password = req.body.password
-    User.lastLogin = req.body.lastLogin
-    User.userRoll = req.body.userRoll
-
-    User.save((err,UserInfoSave) => {
-        if(err) res.status(500).send({message: `Error Al Guardar Usuario : ${err}`})
-
-        res.status(200).send({User : UserInfoSave})
-    })
-})
-
-app.put('/api/product/:productId',(req,res)=>{
-
-})
-
-app.delete('/api/product/:productId',(req,res)=>{
-
-})
 
 mongoose.connect('mongodb://localhost:27017/workApp',(err,res)=>{
     if(err){
