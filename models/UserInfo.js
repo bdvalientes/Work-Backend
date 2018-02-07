bb52379ab2950cf055df4ae807759fd30b7ad6b3
Brayan David Valiente Simbaqueba <bdvalientes@gmail.com>
2018-02-06 20:33:36 -0500

    Funciones De Mongo
    Se Creo Una Funcion para encriptar la passwork y generar avatar automaticos

--- models/UserInfo.js
+++ models/UserInfo.js
@@ -10,6 +10,7 @@ const UserShema = new Shema({
         lowercase: true
     },
     displayName: String,
+    avatar:String,
     phone:String,
     puntuation: {
         type :Number,
@@ -31,5 +32,30 @@ const UserShema = new Shema({
         enum: ['Student', 'Employee','Student and Employee']
     }
 })
+
+UserShema.pre('save',(next)=>{
+    let user = this
+    if(!user.isModified('password')) return next()
+
+    bcrypy.genSalt(10,(err,salt)=>{
+        if(err) return next(err)
+
+        bcrypy.hash(user.password,salt,null,(err,hash)=>{
+            if(err) return next(err)
+
+            user.password = hash
+            next()
+        })
+    })
+})
+
+UserShema.methods.gravatar = function () {
+    if (!this.email) return `https://gravatar.com/avatar/?s=200&d=retro`
+  
+    const md5 = crypto.createHash('md5').update(this.email).digest('hex')
+    return `https://gravatar.com/avatar/${md5}?s=200&d=retro`
+  }
+
+
 
 module.exports = mongoose.model('UserInfo', UserShema)
