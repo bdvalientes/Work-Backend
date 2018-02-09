5d163af948a9e46e8032e6ffd72aeeb4a16e47cf
Brayan David Valiente Simbaqueba <bdvalientes@gmail.com>
2018-02-08 21:05:34 -0500

    Create and Decode
    Se creo las funciones que crean un Token y lo decodifican

---
+++ services/index.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const jwt = require('jwt-simple')
+const moment = require('moment')
+const config = require('../config')
+
+
+//Creacion De Tokens
+function createToken(user){
+    const payload = {
+        sub: user,
+        iat: moment().unix(),
+        exp: moment().add(1,'month').unix(),
+    }
+
+    return jwt.encode(payload,config.SECRET_TOKEN)
+}
+
+
+//Decodificacion De Token 
+function decodeToken (token){
+    const decode = new Promise((resolve,reject)=>{
+        try {
+            const payload = jwt.decode(token,config.SECRET_TOKEN)
+            if(payload.exp <= moment().unix()){
+                reject({
+                    status: 401,
+                    message: `El Token Ha Expirado`    
+                })
+            }
+
+            resolve(payload.sub)
+        } catch (err) {
+            reject({
+                status: 500,
+                message: `Invalid Token`
+            })            
+        }
+    })
+
+    return decode
+}
+
+module.exports ={
+    createToken,
+    decodeToken
+}
